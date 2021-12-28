29943eea9cca08dc5375e97356a8f1810bc9b995
Muhammad Bilal <muhammadbilal20130@gmail.com>
2021-12-28 20:46:16 +0500

    some final UI touches to recycler view

--- app/src/main/java/com/example/foodmenuapp/MainActivity.java
+++ app/src/main/java/com/example/foodmenuapp/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ArrayList<MenuModel> menuList=new ArrayList<>();
         menuList.add(new MenuModel(R.drawable.buger,"Burger","399","This is description"));
         menuList.add(new MenuModel(R.drawable.buger,"Burger","399","This is description"));
