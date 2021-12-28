16913b9927e51a9e56a9dfb3695028636a7537e3
Muhammad Bilal <muhammadbilal20130@gmail.com>
2021-12-28 20:36:02 +0500

    added recyclerview in main activity main

--- app/src/main/java/com/example/foodmenuapp/MainActivity.java
+++ app/src/main/java/com/example/foodmenuapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.foodmenuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.foodmenuapp.adapter.MenuAdapter;
+import com.example.foodmenuapp.model.MenuModel;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<MenuModel> menuList=new ArrayList<>();
+        menuList.add(new MenuModel(R.drawable.buger,"Burger","399","This is description"));
+        menuList.add(new MenuModel(R.drawable.buger,"Burger","399","This is description"));
+        menuList.add(new MenuModel(R.drawable.buger,"Burger","399","This is description"));
+        menuList.add(new MenuModel(R.drawable.buger,"Burger","399","This is description"));
+        menuList.add(new MenuModel(R.drawable.buger,"Burger","399","This is description"));
+        menuList.add(new MenuModel(R.drawable.buger,"Burger","399","This is description"));
+        //Setting Recycler View adatper
+        RecyclerView recyclerView=findViewById(R.id.recylerView);
+        MenuAdapter adapter=new MenuAdapter(menuList,this);
+//        recyclerView.setLayoutManager(recyclerView.getLayoutManager());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
