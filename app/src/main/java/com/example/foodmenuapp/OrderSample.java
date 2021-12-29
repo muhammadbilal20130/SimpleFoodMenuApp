cc02d39d82e91a38be9ba8dc5614f18a967d2549
Muhammad Bilal <muhammadbilal20130@gmail.com>
2021-12-29 20:52:05 +0500

    Completed Order list activity using recycler view

--- app/src/main/java/com/example/foodmenuapp/OrderSample.java
+++ app/src/main/java/com/example/foodmenuapp/OrderSample.java
@@ -1,8 +1,16 @@
 package com.example.foodmenuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.foodmenuapp.adapter.MenuAdapter;
+import com.example.foodmenuapp.adapter.OrdersSampleAdapter;
+import com.example.foodmenuapp.model.OrdersModel;
+
+import java.util.ArrayList;
 
 public class OrderSample extends AppCompatActivity {
 
@@ -10,6 +18,14 @@ public class OrderSample extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_sample);
+        ArrayList<OrdersModel> ordersModelArrayList=new ArrayList<>();
+        ordersModelArrayList.add(new OrdersModel(R.id.image,"Pizza","AC-11","425"));
+        ordersModelArrayList.add(new OrdersModel(R.id.image,"Pizza","AC-11","425"));
+        ordersModelArrayList.add(new OrdersModel(R.id.image,"Pizza","AC-11","425"));
+        RecyclerView recyclerView=findViewById(R.id.OrderRecyclerView);
+        OrdersSampleAdapter ordersSampleAdapter=new OrdersSampleAdapter(ordersModelArrayList,this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(ordersSampleAdapter);
 
     }
 }
